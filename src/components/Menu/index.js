dd6781cc6565dd93efcc3b80a598d91a1a1a54fb
Ali Zand <ali.zand@realpage.com>
2020-05-21 13:41:40 -0700

    Make menu look nice

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,16 +5,30 @@ import Nav from 'react-bootstrap/Nav';
 class Menu extends React.Component {
   render() {
     return (
-      <Navbar variant="light">
-        <Navbar.Brand>
-          <h1>MEHRNAZ NOBARINIA</h1>
-        </Navbar.Brand>
-        <Nav className="ml-auto">
-          <Nav.Link active={true}><span className="menu-item">Work</span></Nav.Link>
-          <Nav.Link><span className="menu-item">Exhibition</span></Nav.Link>
-          <Nav.Link><span className="menu-item">About</span></Nav.Link>
-          <Nav.Link><span className="menu-item">Contact</span></Nav.Link>
-        </Nav>
+      <Navbar variant="light" expand="lg">
+        <span className="menu-container">
+          <Navbar.Brand>
+            <h1>MEHRNAZ NOBARINIA</h1>
+          </Navbar.Brand>
+        </span>
+        <span className="menu-container">
+          <Navbar.Toggle aria-controls="menu" />
+        </span>
+
+        <span className="menu-container-xs">
+          <Navbar.Brand>
+            <h1>MEHRNAZ NOBARINIA</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="menu" />
+        </span>
+        <Navbar.Collapse id="menu">
+          <Nav className="ml-auto">
+            <Nav.Link active={true}><span className="menu-item">Work</span></Nav.Link>
+            <Nav.Link><span className="menu-item">Exhibitions</span></Nav.Link>
+            <Nav.Link><span className="menu-item">About</span></Nav.Link>
+            <Nav.Link><span className="menu-item">Contact</span></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     );
   }

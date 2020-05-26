a630b684ab428f034ab7e72ed373022c4ebd1984
Ali Zand <ali.zand@realpage.com>
2020-05-25 21:15:12 -0700

    Install Draft JS

--- src/components/About/Edit/index.js
+++ src/components/About/Edit/index.js
@@ -1,15 +1,52 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import { Editor, EditorState } from 'draft-js';
 
 class Edit extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+
+    this.onChange = (editorState) => this.setState({editorState});
+
+    this.setEditor = (editor) => {
+      this.editor = editor;
+    };
+
+    this.focusEditor = () => {
+      if (this.editor) {
+        this.editor.focus();
+      }
+    };
+  }
+
+  componentDidMount() {
+    this.focusEditor();
+  }
+
   render() {
     return (
-      <Row>
-        <Col>
-
-        </Col>
-      </Row>
+      <div style={styles.editor}>
+        <Editor
+          ref={this.setEditor}
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+        />
+      </div>
     );
   }
 }
+
+const styles = {
+  editor: {
+    border: '1px solid gray',
+    minHeight: '6em'
+  }
+};
 
 export default Edit;
